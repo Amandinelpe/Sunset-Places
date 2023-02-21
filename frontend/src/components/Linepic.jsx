9fd4ef8ccf9ec90b3c80209d8c191f9e052973f1
Amandine Leporace <leporace.amandine@gmail.com>
2023-02-21 11:40:42 +0100

    home slide

--- frontend/src/components/Linepic.jsx
+++ frontend/src/components/Linepic.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Linepic.css";
 import Dune from "../assets/Dune.png";
 import Sunset from "../assets/sunset.jpg";
-import Cassis from "../assets/Cassis.png";
+import Nice from "../assets/Nice.jpg";
 import Mont from "../assets/mont_blanc.jpg";
 import Paris from "../assets/pont_paris.jpg";
 
@@ -12,7 +12,7 @@ function Linepic() {
       <div className="gallery">
         <img src={Dune} alt="Dune" />
         <img src={Sunset} alt="plage" />
-        <img src={Cassis} alt="plage" />
+        <img src={Nice} alt="plage" />
         <img src={Mont} alt="plage" />
         <img src={Paris} alt="plage" />
       </div>
